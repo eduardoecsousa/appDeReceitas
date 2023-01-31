e959048c61029b97ec3dff6246e6b74941bfad13
Isadora Araújo <isadora.sga@gmail.com>
2023-01-31 19:22:20 -0300

    trabalhando em pagina de receitas feitas

--- src/pages/FavoriteRecipes.jsx
+++ src/pages/FavoriteRecipes.jsx
@@ -2,15 +2,38 @@ import PropTypes from 'prop-types';
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { changeTile } from '../redux/actions';
+import ButtonCompFavor from '../components/ButtonCompFavor';
 
 function FavoriteRecipes({ dispatch }) {
+  const favorites = JSON.parse(localStorage.getItem('favoriteRecipes'));
+  // const keyObjId = mealsOrDrinks === 'drinks' ? 'idMeal' : 'idDrink';
+  // const keyObjName = mealsOrDrinks === 'drinks' ? 'strMeal' : 'strDrink';
+  // const keyObjImg = mealsOrDrinks === 'drinks' ? 'strMealThumb' : 'strDrinkThumb';
+
   useEffect(() => {
     dispatch(changeTile('Favorite Recipes'));
   }, []);
 
   return (
     <div>
-      favorite
+      { favorites.map((favorite) => (
+        <div
+          key={ favorite.name }
+        >
+          <img
+            src={ favorite.image }
+            alt={ favorite.name }
+            // data-testid={ `${index}-horizontal-image` }
+            style={ {
+              maxWidth: '360px',
+            } }
+          />
+          <h5>{favorite.name}</h5>
+          <p>{favorite.nationality}</p>
+          <p>{favorite.type}</p>
+          <ButtonCompFavor shareUrl={ shareUrl } />
+        </div>
+      ))}
     </div>
   );
 }
